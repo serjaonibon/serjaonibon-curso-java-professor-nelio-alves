a87fc43a479f5f435574152f82dd0203abfdc4d2
Sergio Nibon <snibonjr@gmail.com>
2024-10-05 01:25:32 -0300

    Atualização

--- src/exercicio_02.java
+++ src/exercicio_02.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
+import java.util.Locale;
 public class exercicio_02 {
+    public static void main(String[] args) {
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+        //Locale.setDefault(Locale.US);
+        System.out.printf("%.2f%n", y);
+    }
 }
--- src/exercicio_04.java
+++ src/exercicio_04.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class exercicio_04 {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double)  a / b; //casting
+
+        System.out.println(resultado);
+    }
 }
